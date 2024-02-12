29864d39c614ce067ccf64c9e8b24098cf9a57bb
REDMONKEYGOD <jeanpaulk701@gmail.com>
2024-02-12 18:37:52 +0000

    Apopos

--- frontend/src/pages/Apropos.tsx
+++ frontend/src/pages/Apropos.tsx
@@ -4,11 +4,9 @@ import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -49,16 +47,12 @@ export default function SignIn() {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            borderRadius: '10px', // Ajoute des coins arrondis
-            backgroundImage: 'url("chemin/vers/votre/image.jpg")', // Remplacez le chemin par votre image
-            backgroundSize: 'cover', // Ajuste la taille de l'image pour couvrir tout le formulaire
-            padding: '20px', // Ajoute un espace intérieur pour que le contenu ne soit pas trop proche des bords
-            backgroundColor: '#f0f0f0', // Remplacez la couleur par celle que vous souhaitez
+            borderRadius: '10px',
+            backgroundSize: 'cover',
+            padding: '20px',
+            backgroundColor: '#f0f0f0',
           }}
         >
-
-
-
           <Typography component="h1" variant="h5">
             bonjour!
           </Typography>
