b38c5cf6f7fb5a1a08d9b87967f801cfb9d848d6
joceOG <jocelynboka@gmail.com>
2024-05-09 20:35:19 +0000

    Change BD

--- backend/models/service.js
+++ backend/models/service.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const ServiceSxchema = mongoose.Schema({
+const ServicexSchema = mongoose.Schema({
     nom: { type: String, required: true },
     prix: { type: String, required: true },
     image: {
         type: Buffer,
     },
+    
 });
 
-module.exports = mongoose.model('Servicex', ServiceSxchema);
+module.exports = mongoose.model('Servicex', ServicexSchema);
