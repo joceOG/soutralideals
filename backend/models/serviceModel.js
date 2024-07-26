d12a89eaa0e297d96055add2833b4f0f896a94c2
oguoronou voss <alexkouame99@gmail.com>
2024-07-26 23:11:50 +0000

    creation utilisateur et authentification

--- backend/models/serviceModel.js
+++ backend/models/serviceModel.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const ServiceSchema = mongoose.Schema({
-    nomservice: { type: String, required: true },
-    imageservice: {
+const ServicexSchema = mongoose.Schema({
+    nom: { type: String, required: true },
+    prix: { type: String, required: true },
+    image: {
         type: Buffer,
     },
-    idcategorie:{type: String, required: true},
-    nomcategorie : { type: String, required: true },
+    
 });
 
-module.exports = mongoose.model('Service', ServiceSchema);
+module.exports = mongoose.model('Servicex', ServicexSchema);
