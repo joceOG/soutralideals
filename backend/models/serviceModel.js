0e04b7ff5c411691a8549ef8ff3b2fe166c85c27
Ncir Jaweher <jaweherncir@gmail.com>
2024-09-17 21:17:05 +0100

    PR[Gestion des services]

--- backend/models/serviceModel.js
+++ backend/models/serviceModel.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose');
 
-const ServiceSchema = mongoose.Schema({
+const ServiceSchema = new mongoose.Schema({
     nomservice: { type: String, required: true },
-    imageservice: {
-        type: Buffer,
-    },
-    categorie: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Categorie',
-        required: true 
-    },
-    
-});
+    imageservice: { type: String, required: true },
+    categorie: { type: mongoose.Schema.Types.ObjectId, ref: 'Categorie', required: true },
+    nomgroupe: { type: mongoose.Schema.Types.ObjectId, ref: 'Groupe', required: true } // Adjust as necessary
+  });
+  
 
 module.exports = mongoose.model('Service', ServiceSchema);
 
