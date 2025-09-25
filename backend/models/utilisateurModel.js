ec1d0fd7f76fb6591b1c5c7f7b7b015a006ec19c
joceOG <51080560+joceOG@users.noreply.github.com>
2025-09-25 11:15:04 +0000

    Update utilisateurModel.js

--- backend/models/utilisateurModel.js
+++ backend/models/utilisateurModel.js
@@ -17,18 +17,18 @@ const UtilisateurSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-email: {  
-  type: String,
-  trim: true,
-  lowercase: true,
-  unique: true,
-  sparse: true, // ✅ important quand unique mais pas obligatoire
-  validate(value) {
-    if (value && !validator.isEmail(value)) { 
-      throw new Error('Email invalide');
+  email: {  
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: false,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email invalide');
+      }
     }
-  }
-},
+  },
   password: { 
     type: String,
     required: true,
